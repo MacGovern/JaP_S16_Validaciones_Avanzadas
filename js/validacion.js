9a051adaf5ec49490644270c751b5637753f134c
lg <leonardoagustingimenez@gmail.com>
2023-10-26 16:53:04 -0300

    Finalizado

--- js/validacion.js
+++ js/validacion.js
@@ -1,16 +1,34 @@
-(() => {
-    'use strict';  
-    
-    const forms = document.querySelectorAll('.needs-validation');  
-   
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    });
-  });
+const form = document.getElementById('form');
+const modalFeedback = document.getElementById('modalFeedback');
+const modalBtn = document.getElementById('modalBtn');
+
+function updatePassword2Pattern(password1) {
+  form.password2.pattern = password1.value;
+}
+
+function siwtchTerminosFeedback() {
+  if (form.terminos.checkValidity()) {
+    modalFeedback.classList.replace('d-block', 'd-none');
+    modalBtn.classList.remove('text-danger');
+  } else {
+    modalFeedback.classList.replace('d-none', 'd-block');
+    modalBtn.classList.add('text-danger');
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  if (!form.checkValidity()) {
+    e.preventDefault();
+    form.classList.add('was-validated');
+    if (!form.terminos.checkValidity()) {
+      modalFeedback.classList.replace('d-none', 'd-block');
+      modalBtn.classList.add('text-danger');
+    }
+    form.terminos.addEventListener('click', siwtchTerminosFeedback);
+  } else {
+    form.reset();
+    form.classList.remove('was-validated');
+    form.terminos.removeEventListener('click', siwtchTerminosFeedback);
+    alert('Formulario enviado.');
+  }
+});
